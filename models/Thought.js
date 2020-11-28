7ccb47e6d1382b7c87348920b1a94652894c6b55
hameed kazeem <hameed.kazeem@gmail.com>
2020-11-28 09:28:24 -0500

    created models for DB

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reaction = require('./Reaction')
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reaction]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
